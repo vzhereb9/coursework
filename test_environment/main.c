0a82b2c43b8d37fc9252894995cd3d64d8a5fc41
vzhereb9 <56634254+vzhereb9@users.noreply.github.com>
2020-03-05 05:21:28 +0300

    Update main.c

--- test_environment/main.c
+++ test_environment/main.c
@@ -1,10 +1,30 @@
-#include <stdio.h>
-#include <time.h>
-#include "test_env.h"
+#include "main.h"
 
 int main() {
-    double speed;
-    speed = run_test_env();
-    printf("Average speed is %.2f KB/s\n", speed);
+    srand(time(NULL));
+    unsigned int flag = 0;
+    double avg_speed_calc[16], avg_speed_recover[16];
+    memset(avg_speed_calc,0,sizeof(double)*16);
+    memset(avg_speed_recover,0,sizeof(double)*16);
+    find_avg_speeds(avg_speed_calc, avg_speed_recover, flag);
+    for (int r = 4; r <= 4; r += 4)
+    {
+        printf("Average classic calc speed for %u drives is %.2f kB/s\n", r, avg_speed_calc[(r / 4) - 1]);
+    }
+    for (int r = 4; r <= 4; r += 4)
+    {
+        printf("Average classic recover speed for %u drives is %.2f KB/s\n", r, avg_speed_recover[(r / 4) - 1]);
+    }
+
+    flag = 1;
+    find_avg_speeds(avg_speed_calc, avg_speed_recover, flag);
+    for (int r = 4; r <= 4; r += 4)
+    {
+        printf("Average vector calc speed for %u drives is %.2f kB/s\n", r, avg_speed_calc[(r / 4) - 1]);
+    }
+    for (int r = 4; r <= 4; r += 4)
+    {
+        printf("Average vector recover speed for %u drives is %.2f KB/s\n", r, avg_speed_recover[(r / 4) - 1]);
+    }
     return 0;
 }
