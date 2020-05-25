09d569ae7bddb366e60ae7647c90d9be7efb7485
vzhereb9 <56634254+vzhereb9@users.noreply.github.com>
2020-05-25 15:06:43 +0300

    Add files via upload

--- coursework_SSE/main.c
+++ coursework_SSE/main.c
@@ -3,9 +3,7 @@
 int main() {
     srand(time(NULL));
     unsigned int flag = 0;
-    double avg_speed_calc[16], avg_speed_recover_one_drive[16], avg_speed_recover[16];
-    memset(avg_speed_calc,0,sizeof(double)*16);
-    memset(avg_speed_recover,0,sizeof(double)*16);
+    double avg_speed_calc[16][3], avg_speed_recover_one_drive[16][3], avg_speed_recover[16][3];
 	
 	FILE *file;
         file = fopen("output.txt", "w");
@@ -13,76 +11,77 @@ int main() {
     find_avg_speeds(avg_speed_calc, avg_speed_recover_one_drive, avg_speed_recover, flag);
     for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average classic calc speed for %u drives is %.2f MB/s\n", r, avg_speed_calc[(r / 4) - 1]);
+		printf("Classic calc speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_calc[(r / 4) - 1][0], avg_speed_calc[(r / 4) - 1][1], avg_speed_calc[(r / 4) - 1][2]);
     }
 	for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average classic recover one drive speed for %u drives is %.2f MB/s\n", r, avg_speed_recover_one_drive[(r / 4) - 1]);
+        printf("Classic recover one drive speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_recover_one_drive[(r / 4) - 1][0], avg_speed_recover_one_drive[(r / 4) - 1][1], avg_speed_recover_one_drive[(r / 4) - 1][2]);
     }
     for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average classic recover two drives speed for %u drives is %.2f MB/s\n", r, avg_speed_recover[(r / 4) - 1]);
+        printf("Classic recover two drives speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_recover[(r / 4) - 1][0], avg_speed_recover[(r / 4) - 1][1], avg_speed_recover[(r / 4) - 1][2]);
     }
 
 	fputs("Calc classic: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_calc[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_calc[(r / 4) - 1][2]);
 	fputs("\nRecover one drive classic: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_recover_one_drive[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_recover_one_drive[(r / 4) - 1][2]);
 	fputs("\nRecover two drives classic: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_recover[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_recover[(r / 4) - 1][2]);
 
     flag = 1;
     find_avg_speeds(avg_speed_calc, avg_speed_recover_one_drive, avg_speed_recover, flag);
     for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average vector calc speed for %u drives is %.2f MB/s\n", r, avg_speed_calc[(r / 4) - 1]);
+		printf("Vector calc speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_calc[(r / 4) - 1][0], avg_speed_calc[(r / 4) - 1][1], avg_speed_calc[(r / 4) - 1][2]);
     }
 	for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average vector recover one drive speed for %u drives is %.2f MB/s\n", r, avg_speed_recover_one_drive[(r / 4) - 1]);
+        printf("Vector recover one drive speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_recover_one_drive[(r / 4) - 1][0], avg_speed_recover_one_drive[(r / 4) - 1][1], avg_speed_recover_one_drive[(r / 4) - 1][2]);
     }
     for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average vector recover two drives speed for %u drives is %.2f MB/s\n", r, avg_speed_recover[(r / 4) - 1]);
+        printf("Vector recover two drives speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_recover[(r / 4) - 1][0], avg_speed_recover[(r / 4) - 1][1], avg_speed_recover[(r / 4) - 1][2]);
     }
 
 	fputs("\n\nCalc vector: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_calc[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_calc[(r / 4) - 1][2]);
 	fputs("\nRecover one drive vector: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_recover_one_drive[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_recover_one_drive[(r / 4) - 1][2]);
 	fputs("\nRecover two drives vector: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_recover[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_recover[(r / 4) - 1][2]);
 
     flag = 2;
     find_avg_speeds(avg_speed_calc, avg_speed_recover_one_drive, avg_speed_recover, flag);
     for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average RAIDIX calc speed for %u drives is %.2f MB/s\n", r, avg_speed_calc[(r / 4) - 1]);
+		printf("RAIDIX calc speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_calc[(r / 4) - 1][0], avg_speed_calc[(r / 4) - 1][1], avg_speed_calc[(r / 4) - 1][2]);
     }
 	for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average RAIDIX recover one drive speed for %u drives is %.2f MB/s\n", r, avg_speed_recover_one_drive[(r / 4) - 1]);
+        printf("RAIDIX recover one drive speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_recover_one_drive[(r / 4) - 1][0], avg_speed_recover_one_drive[(r / 4) - 1][1], avg_speed_recover_one_drive[(r / 4) - 1][2]);
     }
     for (int r = 4; r <= 64; r += 4)
     {
-        printf("Average RAIDIX recover two drives speed for %u drives is %.2f MB/s\n", r, avg_speed_recover[(r / 4) - 1]);
+        printf("RAIDIX recover two drives speed for %u drives med is %.2f MB/s, margin of error is %.2f MB/s, avg speed is %.2f MB/s\n", r, avg_speed_recover[(r / 4) - 1][0], avg_speed_recover[(r / 4) - 1][1], avg_speed_recover[(r / 4) - 1][2]);
     }
 
 	fputs("\n\nCalc RAIDIX: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_calc[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_calc[(r / 4) - 1][2]);
 	fputs("\nRecover one drive RAIDIX: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_recover_one_drive[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_recover_one_drive[(r / 4) - 1][2]);
 	fputs("\nRecover two drives RAIDIX: \n", file);
 	for (int r = 4; r <= 64; r += 4)
-		fprintf(file, "%.2f, ", avg_speed_recover[(r / 4) - 1]);
+		fprintf(file, "%.2f, ", avg_speed_recover[(r / 4) - 1][2]);
 
+	fclose(file);
     return 0;
 }
